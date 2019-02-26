d6e25469a4318e3e03217cefd082018c874584b3
kwonl <dlrnjsgud322@gmail.com>
2019-02-26 20:52:37 +0900

    Solved my first blog

---
+++ 2019/mitre_CTF/150_my_first_blog/README.md
@@ -0,0 +1,11 @@
+# My First Blog
+
+## Description
+
+http://138.247.13.106/
+
+## Solution
+
+Use distributed version control system ripper
+
+`MCA{canonical_is_literally_my_favorite_company_in_the_whole_world}`
---
+++ 2019/mitre_CTF/150_my_first_blog/index.php
@@ -0,0 +1,26 @@
+<html>
+    <head>
+        <title>My Blog</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    </head>
+    <body>
+
+        <div class="container">
+        <div class="jumbotron">
+            <h1 class="display-4">My Blog</h1>
+            <p class="lead">Just a spot for me to talk about how much I love Canonical</p>
+        </div>
+            <h1>I love Canonical</h1>
+            <p>As someone who is just getting started with Linux, I love Canonical. They build the easiest to use Linux distribution I can find, and they build so many useful tools. So far I've tried out</p>
+            <ul>
+                <li>Juju - The worlds best configuration management tool!</li>
+                <li>Bazaar - The worlds best version control!</li>
+                <li>Ubuntu - The worlds best OS!</li>
+                <li>Launchpad - GitHub? Gross!</li>
+            </ul>
+            <hr>
+            <h1>Learning PHP</h1>
+            <p>I recently learned about PHP and I can't stop switching everything over to it. In fact, this blog is now powered by PHP, I think! I changed the file extension at least, and added a little PHP code below here. That should pretty much do it right? I have the PHP code commented out for now since I can't seem to get it to work right. I'll have to look into it later.</p>
+                    </div>
+    </body>
+</html>
